57dfc9ed17e0f776196162f864d644750834dcbb
chenguizhi <1570642298@qq.com>
2021-05-19 17:23:50 +0800

    老妖的springcloud

---
+++ service-ribbon/src/main/java/com/monster/serviceribbon/controller/HelloControler.java
@@ -0,0 +1,23 @@
+package com.monster.serviceribbon.controller;
+
+import com.monster.serviceribbon.sevice.impl.HelloServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+    private static final Logger logger = LoggerFactory.getLogger(HelloControler.class);
+    @Autowired
+    HelloServiceImpl helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        logger.info("Hellow world!");
+        return helloService.sayHi(name);
+    }
+}
+
---
+++ service-ribbon/src/main/java/com/monster/serviceribbon/sevice/HelloService.java
@@ -0,0 +1,5 @@
+package com.monster.serviceribbon.sevice;
+
+public interface HelloService {
+    String sayHi(String name);
+}
